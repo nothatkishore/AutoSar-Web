58e66d31be4127c9230164a012d5550d415d7e58
kishoear <kishorear.programming@gmail.com>
2024-09-29 00:06:44 +0530

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-slate-950 h-screen w-screen flex justify-center items-center">
+      <h1 className='bg-slate-800 text-white p-6 rounded-md border border-blue-300'>Venky Bro is ultimate</h1>
+    </div>
+  );
+};
+
+export default App;
