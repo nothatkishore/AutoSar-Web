2e4cdbfc0cc9b691402cb906079cf585b9f3150b
kishoear <kishorear.programming@gmail.com>
2024-09-29 19:22:18 +0530

    Sample Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="bg-slate-950 h-screen w-screen flex justify-center items-center">
-      <h1 className='bg-slate-800 text-white p-6 rounded-md border border-blue-300'>Venky Bro is ultimate</h1>
+    <div className="bg-[#F5F7F8] h-screen w-screen flex justify-center items-center gap-2 p-2">
+      <div className='bg-slate-900 w-1/2 h-full rounded-xl flex flex-col gap-2 p-2'>
+        <div className='w-full bg-[#F5F7F8] h-1/2 rounded-lg flex justify-center items-center'>
+          <h1>Layout 1</h1>
+        </div>
+
+        <div className='w-full bg-[#F5F7F8] h-1/2 rounded-lg flex justify-center items-center'>
+          <h1>Layout 2</h1>
+        </div>
+      </div>
+      <div className='bg-slate-900 w-1/2 h-full rounded-xl'>
+        
+      </div>
     </div>
   );
 };
