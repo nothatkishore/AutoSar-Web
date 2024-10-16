db0d2e271ddf280991627b491bb9474f1cd90cb1
kishoear <kishorear.programming@gmail.com>
2024-10-16 23:24:15 +0530

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,61 @@
 import React from 'react';
-import { Card, CardHeader, CardContent, CardActions, Typography, Button, CardMedia } from '@mui/material';
+import speed from '../image/speedometer.png'
 
 const App = () => {
   return (
     <>
-      <div className='h-screen w-screen bg-slate-100 flex justify-center items-center'>
-
-        <div className='bg-slate-500 w-1/2 h-full'>
-
-          <div className='bg-red-400 h-1/2 flex flex-col justify-center items-center'>
-
-            <div className='bg-pink-300 w-full h-1/4 p-10'>
-              <h1 className='text-center text-3xl'>Title goes here</h1>
+      <div className='h-screen w-screen bg-slate-50 flex flex-col'>
+        <div className='text-center'></div>
+        <div className='h-1/2 flex flex-col p-10 justify-center'>
+          <div className='bg-slate-100 border h-5/6 rounded-2xl p-5 shadow-lg flex justify-around gap-10 items-center'>
+    
+            <div className=' border w-1/5 h-1/3 rounded-2xl shadow-lg flex justify-around items-center p-5'>
+              <div className='flex flex-col justify-center items-center'>
+                <p className='text-lg font-mono'>Current (A)</p>
+                <p className='text-2xl font-mono'>7.00</p>
+              </div>
             </div>
 
-            <div className='bg-pink-400 w-full h-3/4'>
+            <div className=' border w-1/5 h-1/3 rounded-2xl shadow-lg flex justify-around items-center p-5'>
+              <div className='flex flex-col justify-center items-center'>
+                <p className='text-lg font-mono'>Speed (RPM)</p>
+                <p className='text-2xl font-mono'>1500</p>
+              </div>            
+            </div>
 
+            <div className=' border w-1/5 h-1/3 rounded-2xl shadow-lg flex justify-around items-center p-5'>
+              <div className='flex flex-col justify-center items-center'>
+                <p className='text-lg font-mono'>Voltage (V)</p>
+                <p className='text-2xl font-mono'>230</p>
+              </div>            
+            </div>
+
+            <div className=' border w-1/5 h-1/3 rounded-2xl shadow-lg flex justify-around items-center p-5'>
+              <div className='flex flex-col justify-center items-center'>
+                <p className='text-lg font-mono'>Temperature (K)</p>
+                <p className='text-2xl font-mono'>273</p>
+              </div>            
+            </div>
+
+            <div className=' border w-1/5 h-1/3 rounded-2xl shadow-lg flex justify-around items-center p-5'>
+              <div className='flex flex-col justify-center items-center'>
+                <p className='text-lg font-mono'>Pressure (pa)</p>
+                <p className='text-2xl font-mono'>972</p>
+              </div>            
             </div>
 
           </div>
-
-
-          <div className='bg-red-700 h-1/2'>
-
-          </div>
-
         </div>
 
-        <div className='bg-slate-600 w-1/2 h-full'>
-
-          <div className='bg-red-600 h-1/2 p-10 flex justify-around items-end'>
-            <div className='bg-slate-200 h-1/5 w-1/5 rounded-lg shadow-2xl p-2 flex justify-center'>
-                <div></div>
-                <div></div>
-            </div>
-            <div className='bg-slate-200 h-1/5 w-1/5 rounded-lg shadow-2xl'>
-
-            </div>
-            <div className='bg-slate-200 h-1/5 w-1/5 rounded-lg shadow-2xl'>
-
-            </div>
-            <div className='bg-slate-200 h-1/5 w-1/5 rounded-lg shadow-2xl'>
-
-            </div>
+        <div className='h-1/2 flex p-10 gap-10'>
+          <div className='bg-slate-100 border w-1/2 rounded-2xl shadow-lg'>
+            
           </div>
 
-
-          <div className='bg-red-800 h-1/2'>
-
+          <div className='bg-slate-100 border w-1/2 rounded-2xl shadow-lg'>
+            
           </div>
-
         </div>
-
       </div>
     </>
   );
